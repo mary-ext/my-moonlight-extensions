0dffcd76dd1ca5ebe60c9eb0e219b8a43d8b60d9
Mary <git@mary.my.id>
2026-09-22 10:34:12 +0700

    image-downloader v0.2.0

--- extensions/image-downloader/rolldown.config.ts
+++ extensions/image-downloader/rolldown.config.ts
@@ -3,7 +3,7 @@ import { defineExtensionConfig } from '@mary-ext/moonlight-bundler';
 export default defineExtensionConfig({
 	manifest: {
 		id: 'imageDownloader',
-		version: '0.1.0',
+		version: '0.2.0',
 		apiLevel: 2,
 		environment: 'desktop',
 		meta: {
