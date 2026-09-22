835b9b3737e1541542eba4510da269b81a4a5a83
Mary <git@mary.my.id>
2026-09-22 10:16:11 +0700

    mcp-server 0.2.4

--- extensions/mcp-server/rolldown.config.ts
+++ extensions/mcp-server/rolldown.config.ts
@@ -3,7 +3,7 @@ import { defineExtensionConfig } from '@mary-ext/moonlight-bundler';
 export default defineExtensionConfig({
 	manifest: {
 		id: 'mcpServer',
-		version: '0.2.3',
+		version: '0.2.4',
 		apiLevel: 2,
 		environment: 'desktop',
 		meta: {
